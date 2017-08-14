2a172dc28b15c26d961d6e3f1329bfdd8b6450c0
Vinorcola <vinorcola@gmail.com>
2017-08-14 17:27:54 +0200

    Review exception.

--- exceptions/Exception.hpp
+++ exceptions/Exception.hpp
@@ -7,15 +7,14 @@ class Exception : public QException
 {
     private:
         QString message;
-
-
+        std::string stdMessage;
 
     public:
         Exception(const QString& message);
 
-
-
         const QString& getMessage() const;
+
+        virtual const char* what() const noexcept override;
 };
 
 #endif // EXCEPTION_HPP
