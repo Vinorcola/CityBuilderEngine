1ec0f8778f7fe0ff2551815f616ff8fefa7d8c3f
Vinorcola <vinorcola@gmail.com>
2020-12-12 15:12:49 +0100

    Add a testing map with houses

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char* argv[])
     QApplication application(argc, argv);
 
     auto window(new MainWindow("assets/zeus"));
-    window->loadMap("assets/zeus/maps/testing-b.yaml");
+    window->loadMap("assets/zeus/maps/testing-with-houses.yaml");
     window->showMaximized();
 
     return application.exec();
